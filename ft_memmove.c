b72ea4415e459db50eb18e442bf580662010bf4a
Pierre-Antoine Fournier <pifourni@f1r7s22.clusters.42paris.fr>
2025-11-04 15:55:50 +0100

    TDO test

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,35 @@
 /*   By: pifourni <pifourni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 10:28:39 by pifourni          #+#    #+#             */
-/*   Updated: 2025/11/04 10:39:44 by pifourni         ###   ########.fr       */
+/*   Updated: 2025/11/04 15:20:11 by pifourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memmove(void *dest, const void *src, unsigned int n)
 {
-	 
+	unsigned int	i;
+
+	if (n == 0 || dest == src)
+	{
+		return (dest);
+	}
+	if (dest > src)
+	{
+		i = n;
+		while (i > 0)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i--;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+	}
+	return (dest);
 }
